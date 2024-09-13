60e18e7769c4248803fb420181a897dd2ab26724
Capybara16 <laurentinokris16@gmail.com>
2024-09-13 10:43:53 +0800

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,37 @@
+
+let firstFavNumb = 16; 
+let secondFavNumb = 27; 
+
+
+console.log("Greater than:");
+console.log(firstFavNumb > secondFavNumb);
+
+console.log("Less than:");
+console.log(firstFavNumb < secondFavNumb);
+
+console.log("Greater than or equal to:");
+console.log(firstFavNumb >= secondFavNumb);
+
+console.log("Less than or equal to:");
+console.log(firstFavNumb <= secondFavNumb);
+
+console.log("Strict equality (===):");
+console.log(firstFavNumb === secondFavNumb);
+
+console.log("Loose equality (==):");
+console.log(firstFavNumb == secondFavNumb);
+
+console.log("Strict inequality (!==):");
+console.log(firstFavNumb !== secondFavNumb);
+
+console.log("Loose inequality (!=):");
+console.log(firstFavNumb != secondFavNumb);
+
+console.log("AND:");
+console.log((firstFavNumb > 5) && (secondFavNumb < 10));
+
+console.log("OR:");
+console.log((firstFavNumb == 7) || (secondFavNumb == 5));
+
+console.log("NOT:");
+console.log(!(firstFavNumb == secondFavNumb));
