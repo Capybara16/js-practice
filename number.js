13777017ebe0b060aa60aa50104eb6df29aeb81d
Capybara16 <laurentinokris16@gmail.com>
2024-09-13 10:17:53 +0800

    Create number.js

---
+++ number.js
@@ -0,0 +1,9 @@
+let firstFavNum = 16;
+let secondFavNum = 27;
+
+console.log(FirstFavNum + secondFavNum);
+console.log(FirstFavNum - secondFavNum);
+console,log(FirstFavNum * secondFavNum);
+console.log(FirstFavNum / secondFavNum);
+console.log(FirstFavNum ** secondFavNum);
+          
