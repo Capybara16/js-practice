aefa51ae4ae5dfd1601875eb6d5ee6cc244f6343
Capybara16 <laurentinokris16@gmail.com>
2024-09-13 10:04:50 +0800

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,12 @@
+let name = " Kristine Duga Laurentino";
+
+let WhatDoYouWannaBecomeInYourLife = "programmer"
+
+let gender = "female";
+
+let twitterhandle = "@Kristin78501371";
+
+console.log(name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterhnadle);
